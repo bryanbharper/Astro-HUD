d2204f2b00023199bed4331d149c932e386b2702
Bryan Harper <bryanbharper@gmail.com>
2017-03-06 14:47:38 -0600

    Minor fix in main.py

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from lib import sensors
 from lib import adc
 
 sensor_array = sensors.get_sensors()
+sensor = sensor_array[0]
 
 while True:
-    sensor = sensor_array[0]
     sensor.update( adc.readVoltage(sensor.channel) )
     print '{}\t Voltage= {}\t Value={}'.format(sensor.name, sensor.sensor_read, sensor.display_value)
