babe82588dd09cb31b890f9c44f244c1b0beeb66
Bryan Harper <bryanbharper@gmail.com>
2017-03-06 14:42:19 -0600

    Fix ADC test code.

--- main.py
+++ main.py
@@ -7,4 +7,5 @@ sensor_array = sensors.get_sensors()
 
 while True:
     sensor = sensor_array[0]
+    sensor.update()
     print '{}\t Voltage= {}\t Value={}'.format(sensor.name, sensor.sensor_read, sensor.display_value)
