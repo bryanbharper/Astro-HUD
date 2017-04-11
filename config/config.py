e53ae77a20aecc761f8b978c877f8a24f928d8bc
rlroger13 <rlroger1@go.olemiss.edu>
2017-04-11 15:53:43 -0500

    beginning writing config file

--- config/config.py
+++ config/config.py
@@ -1,58 +1,24 @@
-import configparser
+# creates new config file
 
-##############################
+#filename -> path file for the .ini
+filename = "c:\\config\name.ini"
+#sect -> section name
+sect = 'section'
+#var -> names a variable within a section
+var = 'variable'
+#val1 -> defines the variable a specified value
+val = value
 
-config = configparser.configparser()
+#creates .ini configuration file
+cfgfile = open(filename,'w')
 
-#Default settings for the configfile
-config['DEFAULT']= {}
+#adds section to config file eg. [section]
+Config.add_section(sect)
+#sets variables and values under the specified section
+# eg. [section]
+# eg. var=val
+Config.set(sect,var, val)
 
-
-i_spy=config['settings']
-
-# i_spy sets libraries
-i_spy['Clock']= {}
-i_spy['FrontEnd']= {}
-i_spy['Helpers']= {}
-i_spy['Sensor']= {}
-
-# measu sets the Sensors
-measu=i_spy['Sensor']
-
-# scout sets the CO2 sensor in configfile
-scout=measu['Oxygen']
-
-# might not need scout type
-#   if we keep i_spy. but if
-#   not it's an easy fix
-scout['type']= 'sensor'
-scout['wireless']= 'false'
-scout['pin']= '13'
-scout['display_name']= 'Oxygen'
-scout['hi_bound']= '100'
-scout['lo_bound']= '0'
-scout['hi_thresh']= '100'
-scout['lo_thresh']= '0'
-scout['conv_coef']= '231/100'
-scout['conv_offset']= '3'
-scout['double name']= 'D_name'
-scout['double value']= 'D_val'
-
-# agent sets the CO2 sensor in configfile
-agent=measu['CO2']
-
-agent['type']= 'sensor'
-agent['wireless']= 'true'
-agent['pin']= '7448'
-agent['display_name']= 'CO2'
-agent['hi_bound']= '1000'
-agent['lo_bound']= '2'
-agent['hi_thresh']= '678'
-agent['lo_thresh']= '429'
-agent['conv_coef']= '1.8'
-agent['conv_offset']= '0'
-
-# configfile is ayeScotty.ini
-# named after StarTrek
-with open('ayeScotty.ini', 'wb') as configfile:
-config.write(configfile)
+#writes information to the configuration file
+Config.write(cfgfile)
+cfgfile.close()
