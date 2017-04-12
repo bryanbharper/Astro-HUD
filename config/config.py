1c67b8516173f078643a2868280fe1b3f86f9a40
rlroger13 <rlroger1@go.olemiss.edu>
2017-04-11 19:36:33 -0500

    sensors_config.py
    Sensors_config.py is written ready for GUI interaction..
    
    Config.py is what I'm working on to write any config file (not just for
    sensors). However I am at a road block on how to separate the
    variable_name and the value for the for-loop

--- config/config.py
+++ config/config.py
@@ -1,23 +1,35 @@
 # creates new config file
+def make(sections, variable_name, value):
+"sections and variable_name are both string arrays"
+    sect = sections
+    var_name = variable_name
+    val = value
 
 #filename -> path file for the .ini
-filename = "c:\\config\name.ini"
+filename = "config/sensors_config.ini"
 #sect -> section name
 sect = 'section'
-#var -> names a variable within a section
-var = 'variable'
+#var_name -> names a variable within a section
+var_name = 'variable'
 #val1 -> defines the variable a specified value
 val = value
 
 #creates .ini configuration file
 cfgfile = open(filename,'w')
 
-#adds section to config file eg. [section]
-Config.add_section(sect)
-#sets variables and values under the specified section
+def set_config():
+    for x in sect:
+    #adds section to config file eg. [section]
+    (Config.add_section(x))
+        for y in var_name:
+            Config.set(sect,y,val )
+
+
+
+#sets variable_name and values under the specijfied section
 # eg. [section]
-# eg. var=val
-Config.set(sect,var, val)
+# eg. var_name=val
+Config.set(sect,var_name, val)
 
 #writes information to the configuration file
 Config.write(cfgfile)
